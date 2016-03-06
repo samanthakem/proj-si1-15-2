bcab704ee911955e860c01f27bd60970e08ffc32
Stenio Elson <stenio.araujo@ccc.ufcg.edu.br>
2016-03-06 18:51:37 -0300

    Creating the controller NotificacoesCtrl.js and mocking the action of the button 'Mais'

--- caronae/client/app/js/controllers/NotificacoesCtrl.js
+++ caronae/client/app/js/controllers/NotificacoesCtrl.js
@@ -1,9 +1,20 @@
 'use strict';
 
-'use strict';
-
 var caronaeAppMain = angular.module('caronaeApp');
 
-caronaeAppMain.controller('MainCtrl', function () {
-
-});
+caronaeAppMain.controller('NotificacoesCtrl', ['$scope', function ($scope) {
+	$scope.not = {
+		name: "Stenio Elson",
+		message: "aceitou sua oferta para Carona.",
+		time: new Date()
+	};
+	$scope.notifications = [$scope.not];
+	
+	$scope.more = function() {
+		for (var i = 0; i < 3; i++) {
+			var copy = angular.copy($scope.not);
+			
+			$scope.notifications.push(copy);
+		}
+	}
+}]);
