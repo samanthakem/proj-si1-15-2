a77bab05869bd93b9bcd9588af93f49cd23f7cf7
alineatc <aline.costa.cc@gmail.com>
2016-03-09 19:53:34 -0300

    Atualiza mensagens/labels do sistema para maior intuitividade

--- caronae/client/app/js/controllers/NotificacoesCtrl.js
+++ caronae/client/app/js/controllers/NotificacoesCtrl.js
@@ -5,15 +5,15 @@ var caronaeAppMain = angular.module('caronaeApp');
 caronaeAppMain.controller('NotificacoesCtrl', ['$scope', function ($scope) {
 	$scope.not = {
 		name: "Stenio Elson",
-		message: "aceitou sua oferta para Carona.",
+		message: "aceitou seu pedido de Carona.",
 		time: new Date()
 	};
 	$scope.notifications = [$scope.not];
-	
+
 	$scope.more = function() {
 		for (var i = 0; i < 3; i++) {
 			var copy = angular.copy($scope.not);
-			
+
 			$scope.notifications.push(copy);
 		}
 	}
