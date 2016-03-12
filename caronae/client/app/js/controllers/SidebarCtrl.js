5b9314db9aa2267a026bfbf716b36edfb76b5fbc
Gustavo Queiroz <gustavo.hqo@gmail.com>
2016-03-11 21:43:57 -0300

    Modificação no side bar

--- caronae/client/app/js/controllers/SidebarCtrl.js
+++ caronae/client/app/js/controllers/SidebarCtrl.js
@@ -5,15 +5,24 @@ var caronaeAppMain = angular.module('caronaeApp');
 caronaeAppMain.controller('SidebarCtrl', ["$scope", "$rootScope", function ($scope, $rootScope) {
 	$rootScope.perspective = {
 		driver: false,
-		rider: true
+		rider: false,
+    home: true
 	}
 	$scope.changePerspective = function(user) {
-		if (user == "motorista") {
+
+    if(user ==="home") {
+      $rootScope.perspective.driver = false;
+      $rootScope.perspective.rider = false;
+      $rootScope.perspective.home = true;
+    } else if (user == "motorista") {
 			$rootScope.perspective.driver = true;
 			$rootScope.perspective.rider = false;
+      $rootScope.perspective.home = false;
 		} else {
 			$rootScope.perspective.driver = false;
 			$rootScope.perspective.rider = true;
+      $rootScope.perspective.home = false;
 		}
+
 	}
 }]);
