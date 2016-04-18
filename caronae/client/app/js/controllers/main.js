933b6e1325ded08a4ba7d8fd54e238f8cbfa76dc
Gustavo Queiroz <gustavo.hqo@gmail.com>
2016-04-18 20:22:56 -0300

    Adicionado botao e funcao de Logout do sistema | MERGE 2

--- caronae/client/app/js/controllers/main.js
+++ caronae/client/app/js/controllers/main.js
@@ -2,13 +2,14 @@
 
 var caronaeAppMain = angular.module('caronaeApp');
 
-caronaeAppMain.controller('MainCtrl', function ($rootScope,$scope,$http) {
+caronaeAppMain.controller('MainCtrl', function ($rootScope,$scope,$http,$location) {
   $rootScope.logged = true;
   $scope.user = {};
   $http.get("/app/logged").success(function(data) {
     $scope.user = data;
     $scope.user.imageURL = "http://www.clker.com/cliparts/5/7/4/8/13099629981030824019profile.svg.med.png";
-  }).error(function(data){
+  }).error(function(data, status) {
+    console.log(data, status);
     $scope.go("/");
   });
   console.log($scope.user);
