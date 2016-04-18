8d53785a0e8568504feaedd5bf25e23f8dde4a73
Samantha Monteiro <samanthakem@gmail.com>
2016-04-18 20:39:43 -0300

    adiciona atributos de carona

--- caronae/server/app/model/caronaModel/Carona.java
+++ caronae/server/app/model/caronaModel/Carona.java
@@ -1,5 +1,8 @@
 package model.caronaModel;
 
+import java.util.ArrayList;
+import java.util.List;
+import model.Endereco;
 import model.Entidade;
 
 /**
@@ -8,11 +11,82 @@ import model.Entidade;
 public class Carona extends Entidade {
 	
 	private String id;
-
-	public Carona() {}
 	
-	public Carona(String id) {
+	private List<String> idsPassageiros;
+
+	private String idMotorista;
+
+	private int qntVagasDisponiveis;
+
+	private Endereco pontoInicial;
+
+	private Endereco destino;
+
+	public Carona() {
+		this.idsPassageiros = new ArrayList<String>();
+	}
+	
+	/**
+	 * Construtor da entidade {@Carona}
+	 * @param {String} id
+	 * 		id da carona sendo criada
+	 * @param {String} idMotorista
+	 * 		id do motorista responsavel pela carona
+	 * @param {int} qntVagasDisponiveis
+	 * 		quantidade de vagas disponiveis para carona
+	 * @param {Object} pontoInicial
+	 * 		ponto inicial da carona
+	 * @param {Object} destino
+	 * 		destino da carona
+	 */
+	public Carona(String id, String idMotorista, int qntVagasDisponiveis, Endereco pontoInicial, Endereco destino) {
+		this();
 		setId(id);
+		setIdMotorista(idMotorista);
+		setQntVagasDisponiveis(qntVagasDisponiveis);
+		setPontoInicial(pontoInicial);
+		setDestino(destino);
+	}
+	
+	public List<String> getIdsPassageiros() {
+		return idsPassageiros;
+	}
+	
+	
+	public void setIdsPassageiros(List<String> idsPassageiros) {
+		this.idsPassageiros = idsPassageiros;
+	}
+	
+	public void setDestino(Endereco destino) {
+		this.destino = destino;
+	}
+	
+	public Endereco getDestino() {
+		return destino;
+	}
+	
+	public Endereco getPontoInicial() {
+		return pontoInicial;
+	}
+	
+	public void setPontoInicial(Endereco pontoInicial) {
+		this.pontoInicial = pontoInicial;
+	}
+	
+	public int getQntVagasDisponiveis() {
+		return qntVagasDisponiveis;
+	}
+
+	public void setQntVagasDisponiveis(int qntVagasDisponiveis) {
+		this.qntVagasDisponiveis = qntVagasDisponiveis;
+	}
+
+	public void setIdMotorista(String idMotorista) {
+		this.idMotorista = idMotorista;
+	}
+	
+	public String getIdMotorista() {
+		return idMotorista;
 	}
 	
 	public void setId(String id) {
