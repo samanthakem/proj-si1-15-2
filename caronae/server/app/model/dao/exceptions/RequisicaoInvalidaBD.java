280e4c72ec7f3f8c920c95e2cc078c8d03c2bf37
alineatc <aline.costa.cc@gmail.com>
2016-05-24 23:12:25 -0300

    Cria Pessoa_DAO

---
+++ caronae/server/app/model/dao/exceptions/RequisicaoInvalidaBD.java
@@ -0,0 +1,12 @@
+package model.dao.exceptions;
+
+/**
+ * Created by aline on 24/05/2016.
+ */
+public class RequisicaoInvalidaBD extends RuntimeException {
+
+    public RequisicaoInvalidaBD(String msg) {
+        super(msg);
+    }
+
+}
